d56186eb257681f79fb09385ff4f11d464442053
Shnobs <shnobbs91@gmail.com>
2021-07-23 22:03:19 +0300

    New commit

---
+++ Lesson5/functest.py
@@ -0,0 +1,17 @@
+# import random
+# def greet():
+#     print('Hello')
+#     print('Hell in world')
+# greet()
+
+# with open('text.txt') as file:
+#     content = file.read()
+#     print(content)
+
+# file = open('text.txt')
+# content = file.read()
+# print(content)
+# file.close()
+
+file = open('text2.txt', 'a')
+file.write(str(20000)+'\n')
