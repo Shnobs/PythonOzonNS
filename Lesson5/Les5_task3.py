cfeed212ea5ed4f45dc5f364ea5e5f7e7d09689c
Shnobs <shnobbs91@gmail.com>
2021-07-31 15:37:07 +0300

    New commit

--- Lesson5/Les5_task3.py
+++ Lesson5/Les5_task3.py
@@ -1,7 +1,7 @@
 kotoschetchik = 0
-with open('lesson05_cats_of_ulthar.txt', 'r+') as book:
-    for line in book:
-        if line.count('кошка ') > 0:
-            kotoschetchik += line.count('кошка ')
+with open('lesson05_cats_of_ulthar.txt', 'r+') as book:  # Открываем текстовый файл
+    for line in book:  # Проходим файл по строкам
+        if line.count('кошка ') > 0:  # Ищем строки со словом кошка
+            kotoschetchik += line.count('кошка ')   # Добавляем количество слов к счетчику
             print(line)
-print(kotoschetchik)
+print(f'Количество слов кошка в тексте равно {kotoschetchik}')
