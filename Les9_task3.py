808474b8373ef2ebc922c843b7b478753dd770a3
Shnobs <87329056+Shnobs@users.noreply.github.com>
2021-08-27 09:47:53 +0300

    Add files via upload

--- Les9_task3.py
+++ Les9_task3.py
@@ -1,4 +1,4 @@
 import re
 with open('lesson09_cats_of_ulthar.txt') as book:  # Открываем текстовый файл
-    dict_cats = re.findall(r'кошк\w+', book.read())  # Читаем файл и ищем все совпадения с паттерном, вносим их в список
+    dict_cats = re.findall("[Кк]ош\w+", book.read())  # Читаем файл и ищем все совпадения с паттерном, вносим их в список
 print(f'Количество кошек в тектсе равно {len(dict_cats)}')  # Считаем длину получившегося списка
